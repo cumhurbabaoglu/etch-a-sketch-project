ae34b38c7e245f750e87356beee4349d8e86f2ca
Cumhur Babaoğlu <cbabaoglu.dev@gmail.com>
2026-03-07 11:27:39 +0300

    Create 16x16 grid of square divs and give CSS style to grid cells

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 const container = document.querySelector("#container");
+
+for (let i = 1; i <= 256; i++) {
+    const div = document.createElement("div");
+    div.classList.add("grid-cell");
+    container.appendChild(div);
+
+}
