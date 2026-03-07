0a4b863688540dc26318f5e8e51f09b3841c9751
Cumhur Babaoğlu <cbabaoglu.dev@gmail.com>
2026-03-07 11:58:35 +0300

    Add click event listener to the grid size button

--- script.js
+++ script.js
@@ -11,3 +11,30 @@ for (let i = 1; i <= 256; i++) {
 }
 
 const gridSizePrompt = document.querySelector("#grid-size");
+
+gridSizePrompt.addEventListener("click", () => {
+    const userChoice = prompt("Enter number of squares per side (maximum value is 100)");
+    if (userChoice.trim() === "" || Number.isInteger(+userChoice) === false) {
+        alert("Please enter a numeric value.");
+        return;
+    } else if (+userChoice > 100) {
+        alert("Please enter a value less than 100");
+        return;
+    } else {
+        while (container.firstChild) {
+        container.removeChild(container.firstChild);
+}
+
+      for (let i = 1; i <= ((+userChoice) ** 2); i++) {
+        const div = document.createElement("div");
+        div.classList.add("grid-cell");
+        div.style.height = `${100 / +userChoice}%`;
+        div.style.width = `${100 / +userChoice}%`;
+        container.appendChild(div);
+
+        div.addEventListener("mouseover", (e) => {
+        e.target.style.backgroundColor = "green";
+    })
+      }
+    }
+})
