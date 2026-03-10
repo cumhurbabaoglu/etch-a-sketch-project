29cd6b9aa734a8e8b1c905d9f297de964ee08b9a
Cumhur Babaoğlu <cbabaoglu.dev@gmail.com>
2026-03-11 00:22:47 +0300

    Refactor if statements inside gridSizePrompt event listener
    This update adds a new if control for values less than or equal to zero
    and makes the code cleaner by combining if statements into a single one

--- script.js
+++ script.js
@@ -15,13 +15,10 @@ const gridSizePrompt = document.querySelector("#grid-size");
 
 gridSizePrompt.addEventListener("click", () => {
     const userChoice = prompt("Enter number of squares per side (maximum value is 100)");
-    if (userChoice.trim() === "" || Number.isInteger(+userChoice) === false) {
+    if (userChoice.trim() === "" || Number.isInteger(+userChoice) === false || +userChoice > 100 || +userChoice <= 0) {
         alert("Please enter a numeric value.");
-        return;
-    } else if (+userChoice > 100) {
-        alert("Please enter a value less than 100");
-        return;
-    } else {
+    }
+    else {
         while (container.firstChild) {
         container.removeChild(container.firstChild);
 }
